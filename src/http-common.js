356c68610e173d89695e813f49fa764e8e4b0a63
Anderson Souza <anderson.souza@itbam.org.br>
2020-11-12 15:04:22 -0400

    desafio do módulo 04

---
+++ src/http-common.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+//Define a URL base da origem para consumo do servico
+export default axios.create({
+  baseURL: 'https://anderson-api-grades.herokuapp.com/',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
