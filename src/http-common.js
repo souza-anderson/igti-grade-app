f5969bd1b01b4d6bd486c803c54e74c3722633ab
Anderson Souza <anderson.souza@itbam.org.br>
2020-11-12 17:03:38 -0400

    Procfile removed

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://anderson-api-grades.herokuapp.com/',
+  baseURL: 'https://anderson-api-grades.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
